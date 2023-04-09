3d70c7a9463490046ab85f096fb0a13734f0dd18
Christopher Sura <cdw.sura@gmail.com>
2023-04-09 21:54:28 +0200

    fixed join maybe?

--- pages/join.vue
+++ pages/join.vue
@@ -35,6 +35,8 @@
           <button
             type="submit"
             class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            @click="joinGame"
+            @touchstart="joinGame"
           >
             RAUM BEITRETEN
           </button>
